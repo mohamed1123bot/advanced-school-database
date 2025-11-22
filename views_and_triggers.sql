b8408a590e5c63d56e14342f3906d6b53fb747fe
mohamed1123bot <zhranasamh30@gmail.com>
2025-11-22 16:02:48 +0200

    Add views and trigger for student notifications
    Created views for student count per teacher and average grade per class, and added a trigger function for new student notifications.

--- views_and_triggers.sql
+++ views_and_triggers.sql
@@ -1,4 +1,3 @@
--- View: عدد الطلاب لكل مدرس
 CREATE VIEW students_per_teacher AS
 SELECT t.name AS teacher_name,
        COUNT(s.student_id) AS student_count
@@ -6,7 +5,6 @@ FROM teachers t
 LEFT JOIN students s ON t.teacher_id = s.teacher_id
 GROUP BY t.name;
 
--- View: متوسط درجات كل صف
 CREATE VIEW avg_grade_per_class AS
 SELECT c.class_name,
        AVG(s.grade) AS avg_grade
@@ -15,7 +13,6 @@ JOIN student_classes sc ON c.class_id = sc.class_id
 JOIN students s ON sc.student_id = s.student_id
 GROUP BY c.class_name;
 
--- Trigger: تنبيه عند إضافة طالب جديد
 CREATE OR REPLACE FUNCTION new_student_notification()
 RETURNS TRIGGER AS $$
 BEGIN
